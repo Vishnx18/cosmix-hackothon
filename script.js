0103fdbe339a954c3e4ca59d36477f6424bcfa41
Vishnu Karthick Pandian C <vishnukarthick994@gmail.com>
2026-03-28 05:33:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/simulate', {
+            const response = await fetch('https://cosmix-hackothon.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(payload)
